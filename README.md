197e82f66b2af600276501ae4d8a5a04a17fa77c
plasticuproject <19690649+plasticuproject@users.noreply.github.com>
2018-09-08 12:43:33 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # coinbot
-Coinbase query bot for Discord
+Coinbot is a simple bot that returns basic information from <br />
+Coinbase.com about digital currencies, in USD format. <br />
+
+## Requirments
+Python 3.x and PIP
+```
+pip install coinbase
+pip install -U https://github.com/Rapptz/discord.py/archive/rewrite.zip
+```
+
+## Installation
+Sign up with Coinbase at https://coinbase.com <br />
+Enable an API key at https://coinbase.com/settings/api <br />
+Record your API KEY + SECRET
+
+Head over to https://discordapp.com/developers/applications/me and create a new Discord app. <br />
+Click on "Create Bot User". <br />
+Once done, you can get the secret bot token. <br />
+
+In coinbase.py replace all <...> with appropriate information. <br />
+
+To start bot, run:
+```
+python coinbot.py
+```
+
+To add bot to server add your CLIENT_ID to this URL and visit in browser: <br />
+"https://discordapp.com/oauth2/authorize?client_id=<YOUR_BOT_CLIENT_ID_GOES_HERE>&scope=bot" <br />
+
+## Usage Examples
+When bot is active in server type: <br />
+
+<b>%list</b> to return a list of Coinbase supported digital currencies that can be used as arguments for other functions. <br />
+
+<b>%prices BTC</b> to return a list of current BTC trading prices. <br />
+
+<b>%exchange ETH LTC</b> to return exchange rate between two currencies<br />
+
+<b>%history BCH month</b> to return a percentage of gain or loss from specified period. <br />
+Valid period argurments are <b>hour</b>, <b>day</b>, <b>week</b>, <b>month</b>, and <b>year</b>.
---
+++ coinbot.py
@@ -0,0 +1,162 @@
+'''
+Coinbot is a simple bot that returns basic information from
+Coinbase.com about digital currencies, in USD format.
+'''
+
+import discord
+from discord.ext import commands
+import requests
+from requests.exceptions import RequestException
+from coinbase.wallet.client import Client
+
+
+# discord bot prefix
+bot = commands.Bot(command_prefix='%')
+
+# coinbase account login for api acces
+client = Client('<API KEY>', '<API SECRET>')
+
+# build list of coinbase supported digital currencies
+coins = ['BTC', 'LTC', 'BCH', 'ETH', 'ETC']
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.command()
+async def list(ctx):
+
+    # return list of coinbase supported digital currencies
+    value = ', '.join(coins)
+    embed = discord.Embed(title=value)
+    
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def prices(ctx, coin):
+    # return coin prices
+
+    try:
+        coin = coin.upper()
+        buy = client.get_buy_price(currency_pair = '{}-USD'.format(coin))
+        sell = client.get_sell_price(currency_pair = '{}-USD'.format(coin))
+        spot = client.get_spot_price(currency_pair = '{}-USD'.format(coin))
+        buyAmount = buy.get('amount')
+        sellAmount = sell.get('amount')
+        spotAmount = spot.get('amount')
+        value = 'Buy: ${0}, Sell: ${1}, Spot: ${2}'.format(buyAmount, 
+                                                      sellAmount, spotAmount)
+        embed = discord.Embed(title=value)
+
+    except:
+        embed = discord.Embed(title='ERROR: Please check your syntax and' +
+                                    ' try again', color=0xff0000)
+
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def exchange(ctx, coin_one, coin_two):
+    # return exchange rate from one coin to another
+
+    try:
+        coin_one = coin_one.upper()
+        coin_two = coin_two.upper()
+        getRates = client.get_exchange_rates(currency=coin_one)
+        rate = getRates.get('rates').get(coin_two)
+        try:
+            float(rate)
+            value = '1 {0} is worth {1} {2}'.format(coin_one, rate, coin_two)
+            embed = discord.Embed(title=value)
+        except ValueError:
+            embed = discord.Embed(title='This exchange would not make sense')
+    except:
+        embed = discord.Embed(title='ERROR: Exchange data not available' + 
+                              ' try again',color=0xff0000)
+
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def history(ctx, coin, period):
+    #return percentage of change in coin value
+
+    try:
+        coin = coin.upper()
+        period = period.lower()
+        his = client.get_historic_prices(currency_pair = coin + 
+                                         '-USD', period = period)
+        previous = his.get('prices')[-1].get('price')
+        current = client.get_spot_price(currency_pair = 
+                                        '{}-USD'.format(coin)).get('amount')
+        previous = float(previous)
+        current = float(current)
+        if previous == current:
+            embed = discord.Embed(title=coin + ': 0% change')
+        else:
+            if current - previous < 0:
+                sign = 0
+            elif current - previous > 0:
+                sign = 1
+            value = abs(current - previous) / previous * 100
+        if sign == 1:
+            embed = discord.Embed(title=coin + 
+                                  ': UP {0:.2f}%'.format(value),color=0x008000)
+        elif sign == 0:
+            embed = discord.Embed(title=coin + 
+                            ': DOWN {0:.2f}%'.format(value), color=0xff0000)
+    except:
+        embed = discord.Embed(title='ERROR: Please check your syntax and' +
+                              ' try again',color=0xff0000)
+
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def info(ctx):
+    embed = discord.Embed(title='coinbot', 
+                          description='Coinbase query bot.', color=0xeee657)
+    
+    # give info about you here
+    embed.add_field(name='Author', value='<YOUR NAME>')
+    
+    # Shows the number of servers the bot is member of.
+    embed.add_field(name='Server count', value=f'{len(bot.guilds)}')
+
+    # give users a link to invite this bot to their server
+    embed.add_field(name='Invite', value='https://discordapp.com/oauth2/' + 
+                    'authorize?client_id=<YOUR_BOT_CLIENT_ID>')
+
+    await ctx.send(embed=embed)
+
+
+bot.remove_command('help')
+
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(title='coinbot', description='Coinbase query bot.' +
+                          ' List of commands are:', color=0xeee657)
+    embed.add_field(name='%list', value='Return list of coinbase supported' + 
+                    ' digital currencies', inline=False)
+    embed.add_field(name='%prices', value='Example: "%prices BTC"',
+                    inline=False)
+    embed.add_field(name='%exchange', value='Example: "%exchange BTC ETH"',
+                    inline=False)
+    embed.add_field(name='%history', value='Example: "%history BTC week."' + 
+                    ' Peirod usage limited to hour, day, week, month and year',
+                     inline=False)
+    embed.add_field(name='%info', value='Gives a little info about the bot', 
+                    inline=False)
+    embed.add_field(name='%help', value='Gives this message', inline=False)
+
+    await ctx.send(embed=embed)
+
+
+bot.run('<YOUR_TOKEN_GOES_HERE>')
